533d9294991f2269b55b9e407103af6af3b35c96
loginornot <loginornot@gmail.com>
2019-04-06 11:47:55 -0700

    Delete beginning_git_stuff.sh

--- Mentor-Sessions/beginning_git_stuff.sh
+++
@@ -1,6 +0,0 @@
-git clone git@github.com:loginornot/Thinkful.git
-cd Thinkful
-git checkout -b <whatever you want to name your new branch>
-git add 20181103.py
-git commit -m "added today's python file"
-git push origin <whatever you named your branch> 
